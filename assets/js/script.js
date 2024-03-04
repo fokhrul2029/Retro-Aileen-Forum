f2c4750a3d74fccfa6e996199b7b335fee7647a3
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-03-04 23:25:54 +0600

    added spinner

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,23 @@ function initialValue() {
 
 // All post Functionality
 async function fetchAllPost() {
+  const spinner = document.createElement("div");
+  spinner.setAttribute("id", "spinner");
+  spinner.classList.add(
+    "flex",
+    "justify-center",
+    "items-center",
+    "py-5",
+    "text-center"
+  );
+  spinner.innerHTML = `<span class="loading loading-lg loading-spinner text-success"></span>`;
+  allPost.appendChild(spinner);
   const res = await fetch(
     "https://openapi.programming-hero.com/api/retro-forum/posts"
   );
   const data = await res.json();
-  const allPost = data.posts;
-  displayAllPost(allPost);
+  const allData = data.posts;
+  displayAllPost(allData);
 }
 
 fetchAllPost();
@@ -32,7 +43,7 @@ function displayAllPost(data) {
     <div class="border-2 flex gap-4 py-4 px-5 rounded-xl bg-gray-100">
     <div>
       <div class=" w-20  bg-gray-300 rounded-lg relative">
-        <img class="w-[100%]" src="${post.image}" alt="">
+        <img class="w-[100%] rounded-xl" src="${post.image}" alt="">
         <span
           class="absolute -top-1 -right-1 rounded-full p-2 ${
             post.isActive ? "bg-green-500" : "bg-red-500"
@@ -73,6 +84,7 @@ function displayAllPost(data) {
     `;
     allPost.appendChild(postItem);
   }
+  document.getElementById("spinner").remove();
 }
 
 // Functionality of get remark data
@@ -129,62 +141,76 @@ async function getSearchData(value) {
 
 function displaySearchData(data) {
   allPost.innerHTML = "";
-  if (data.length) {
-    for (post of data) {
-      const postItem = document.createElement("div");
-      postItem.classList.add("my-3");
-      postItem.innerHTML = `
-      <div class="border-2 flex gap-4 py-4 px-5 rounded-xl bg-gray-100">
-      <div>
-        <div class=" w-20  bg-gray-300 rounded-lg relative">
-          <img class="w-[100%]" src="${post.image}" alt="">
-          <span
-            class="absolute -top-1 -right-1 rounded-full p-2 ${
-              post.isActive ? "bg-green-500" : "bg-red-500"
-            }"
-          ></span>
+  const spinner = document.createElement("div");
+  spinner.setAttribute("id", "spinner");
+  spinner.classList.add(
+    "flex",
+    "justify-center",
+    "items-center",
+    "py-5",
+    "text-center"
+  );
+  spinner.innerHTML = `<span class="loading loading-lg loading-spinner text-success"></span>`;
+  allPost.appendChild(spinner);
+  setTimeout(() => {
+    if (data.length) {
+      for (post of data) {
+        const postItem = document.createElement("div");
+        postItem.classList.add("my-3");
+        postItem.innerHTML = `
+        <div class="border-2 flex gap-4 py-4 px-5 rounded-xl bg-gray-100">
+        <div>
+          <div class=" w-20  bg-gray-300 rounded-lg relative">
+            <img class="w-[100%] rounded-xl" src="${post.image}" alt="">
+            <span
+              class="absolute -top-1 -right-1 rounded-full p-2 ${
+                post.isActive ? "bg-green-500" : "bg-red-500"
+              }"
+            ></span>
+          </div>
+        </div>
+        <div class="w-[100%]">
+          <div class="flex flex-col border-b-2 pb-2 border-dashed">
+            <div class="flex gap-3">
+              <span># ${post.category} </span>
+              <span>Author : ${post.author.name} </span>
+            </div>
+            <h1 class="text-xl text-[#12132D] font-bold"> ${post.title}</h1>
+            <p> ${post.description} </p>
+          </div>
+          <div class="flex justify-between items-center">
+            <div class="flex gap-6 pt-2">
+              <div class="flex gap-2 items-center">
+                <i class="bi bi-chat-left"></i>
+                <span>${post.comment_count}</span>
+              </div>
+              <div class="flex gap-2 items-center">
+                <i class="bi bi-eye"></i>
+                <span>${post.view_count}</span>
+              </div>
+              <div class="flex gap-2 items-center">
+                <i class="bi bi-clock"></i>
+                <span>${post.posted_time}</span>
+              </div>
+            </div>
+            <div class="cursor-pointer" onclick="getRemarkData(${post.id})">
+              <img src="assets/images/email 1.svg" alt="" />
+            </div>
+          </div>
         </div>
       </div>
-      <div class="w-[100%]">
-        <div class="flex flex-col border-b-2 pb-2 border-dashed">
-          <div class="flex gap-3">
-            <span># ${post.category} </span>
-            <span>Author : ${post.author.name} </span>
-          </div>
-          <h1 class="text-xl text-[#12132D] font-bold"> ${post.title}</h1>
-          <p> ${post.description} </p>
-        </div>
-        <div class="flex justify-between items-center">
-          <div class="flex gap-6 pt-2">
-            <div class="flex gap-2 items-center">
-              <i class="bi bi-chat-left"></i>
-              <span>${post.comment_count}</span>
-            </div>
-            <div class="flex gap-2 items-center">
-              <i class="bi bi-eye"></i>
-              <span>${post.view_count}</span>
-            </div>
-            <div class="flex gap-2 items-center">
-              <i class="bi bi-clock"></i>
-              <span>${post.posted_time}</span>
-            </div>
-          </div>
-          <div class="cursor-pointer" onclick="getRemarkData(${post.id})">
-            <img src="assets/images/email 1.svg" alt="" />
-          </div>
-        </div>
+      `;
+        allPost.appendChild(postItem);
+      }
+    } else {
+      allPost.innerHTML = `
+      <div class="flex justify-center items-center font-3xl py-5  text-center text-3xl text-red-300 border-red-300 border-2 rounded-2xl">
+        <h1>No data Founded</h1>
       </div>
-    </div>
-    `;
-      allPost.appendChild(postItem);
+      `;
     }
-  } else {
-    allPost.innerHTML = `
-    <div class="flex justify-center items-center font-3xl py-5  text-center text-3xl text-red-300 border-red-300 border-2 rounded-2xl">
-      <h1>No data Founded</h1>
-    </div>
-    `;
-  }
+    document.getElementById("spinner").remove();
+  }, 2000);
 }
 
 // Latest Post Functionality

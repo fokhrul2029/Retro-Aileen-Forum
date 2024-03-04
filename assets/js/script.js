bc89a421310b06f7a843b82452fbe6582b0c10a6
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-03-04 22:53:35 +0600

    added search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const latestPost = document.getElementById("latestPost");
 const allPost = document.getElementById("allPost");
 const watchedContent = document.getElementById("watchedContent");
 const remarkPostCounter = document.getElementById("remarkPostCounter");
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
 
 // counter value
 let counter = 0;
@@ -109,6 +111,82 @@ function displayRemarkData(data, id) {
     }
   }
 }
+
+// Search Functionality
+searchBtn.addEventListener("click", () => {
+  const result = searchInput.value.trim();
+  if (result) {
+    getSearchData(result);
+  }
+});
+async function getSearchData(value) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/retro-forum/posts?category=${value}`
+  );
+  const data = await res.json();
+  displaySearchData(data.posts);
+}
+
+function displaySearchData(data) {
+  allPost.innerHTML = "";
+  if (data.length) {
+    for (post of data) {
+      const postItem = document.createElement("div");
+      postItem.classList.add("my-3");
+      postItem.innerHTML = `
+      <div class="border-2 flex gap-4 py-4 px-5 rounded-xl bg-gray-100">
+      <div>
+        <div class=" w-20  bg-gray-300 rounded-lg relative">
+          <img class="w-[100%]" src="${post.image}" alt="">
+          <span
+            class="absolute -top-1 -right-1 rounded-full p-2 ${
+              post.isActive ? "bg-green-500" : "bg-red-500"
+            }"
+          ></span>
+        </div>
+      </div>
+      <div class="w-[100%]">
+        <div class="flex flex-col border-b-2 pb-2 border-dashed">
+          <div class="flex gap-3">
+            <span># ${post.category} </span>
+            <span>Author : ${post.author.name} </span>
+          </div>
+          <h1 class="text-xl text-[#12132D] font-bold"> ${post.title}</h1>
+          <p> ${post.description} </p>
+        </div>
+        <div class="flex justify-between items-center">
+          <div class="flex gap-6 pt-2">
+            <div class="flex gap-2 items-center">
+              <i class="bi bi-chat-left"></i>
+              <span>${post.comment_count}</span>
+            </div>
+            <div class="flex gap-2 items-center">
+              <i class="bi bi-eye"></i>
+              <span>${post.view_count}</span>
+            </div>
+            <div class="flex gap-2 items-center">
+              <i class="bi bi-clock"></i>
+              <span>${post.posted_time}</span>
+            </div>
+          </div>
+          <div class="cursor-pointer" onclick="getRemarkData(${post.id})">
+            <img src="assets/images/email 1.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+      allPost.appendChild(postItem);
+    }
+  } else {
+    allPost.innerHTML = `
+    <div class="flex justify-center items-center font-3xl py-5  text-center text-3xl text-red-300 border-red-300 border-2 rounded-2xl">
+      <h1>No data Founded</h1>
+    </div>
+    `;
+  }
+}
+
 // Latest Post Functionality
 async function fetchLatestPost() {
   const res = await fetch(
